368543c8f301461bf56fc00254e8b535c7a40d12
yanay goor <yanay.goor@gmail.com>
2023-05-19 13:18:26 +0300

    fixes and support cut at any layer

--- split_first_layer.py
+++ split_first_layer.py
@@ -1,9 +1,8 @@
-import copy
 import re
-from dataclasses import dataclass, field
+from dataclasses import dataclass, field, replace
 from functools import cached_property
 from pathlib import Path
-from typing import List, TypeVar, Iterable, Literal, Union
+from typing import TypeVar, Iterable, Literal
 
 import click
 
@@ -13,7 +12,7 @@ PositioningType = Literal["rel", "abs", "unset"]
 layer_pattern = re.compile(";LAYER:(\d+)")
 type_pattern = re.compile(";TYPE:(\w+)")
 
-DEFAULT_TYPE = "unset"
+DEFAULT_TYPE: str = "unset"
 DEFAULT_POSITIONING: PositioningType = "unset"
 
 
@@ -24,19 +23,22 @@ def first_or_none(ls: Iterable[T]) -> T:
         return None
 
 
-def get_opcode(line: str) -> str:
-    opcode = line.split()[0] if line else []
+def get_opcode(line: str) -> str | None:
+    if not line:
+        # TODO: improve
+        return None
+    opcode = line.split()[0]
     return opcode if not opcode.startswith(";") else ";"
 
 
 @dataclass
 class Segment:
-    layer: Union[int, Literal["unset"]]
+    layer: int | Literal["unset"]
     type: str
     start_e_position: int
     positioning_type: PositioningType = DEFAULT_POSITIONING
     extruder_positioning_type: PositioningType = DEFAULT_POSITIONING
-    lines: List[str] = field(default_factory=list, repr=False)
+    lines: list[str] = field(default_factory=list, repr=False)
 
     @cached_property
     def last_e_position(self):
@@ -58,7 +60,7 @@ class Segment:
         return self.start_e_position
 
 
-def parse_gcode(gcode: List[str]) -> List[Segment]:
+def parse_gcode(gcode: str) -> list[Segment]:
     segments = [Segment(layer="unset", type=DEFAULT_TYPE, start_e_position=0)]
     layer = "unset"
     positioning_type: PositioningType = DEFAULT_POSITIONING
@@ -76,7 +78,7 @@ def parse_gcode(gcode: List[str]) -> List[Segment]:
             )
         )
 
-    for line in gcode:
+    for line in gcode.splitlines():
         if match := layer_pattern.match(line):
             layer = int(match.group(1))
             segment_type = DEFAULT_TYPE  # reset type
@@ -114,35 +116,68 @@ def parse_gcode(gcode: List[str]) -> List[Segment]:
     return segments
 
 
-def to_gcode(segments: List[Segment]) -> List[str]:
-    return [line for segment in segments for line in segment.lines]
+def to_gcode(segments: list[Segment]) -> str:
+    return "\n".join(line for segment in segments for line in segment.lines)
+
+
+def format_attrs(*, layer_start: int, layer_end: int, with_skirt: bool) -> str:
+    attrs: str = (
+        f"_layer_{layer_start}"
+        if layer_start == layer_end
+        else f"_layers_{layer_start}_to_{layer_end}"
+    )
+    if with_skirt:
+        attrs += f"_with_skirt"
+    return attrs
 
 
 @click.command()
-@click.argument("input_path", type=str)
-@click.option("--keep-skirt", is_flag=True, help="keep skirt in both outputs")
-def main(input_path: str, keep_skirt: bool):
-    input_path = Path(input_path)
-    gcode = input_path.read_text().splitlines()
-    main_segments = parse_gcode(gcode)
-    first_layer_segments = []
-    for segment in main_segments:
+@click.argument(
+    "input_path", type=click.Path(exists=True, dir_okay=False, path_type=Path)
+)
+@click.option(
+    "-s", "--split-at", type=int, required=True, help="the layer to split after"
+)
+@click.option(
+    "--keep-skirt", is_flag=True, default=False, help="keep skirt in both outputs"
+)
+def main(input_path: Path, keep_skirt: bool, split_at: int):
+    segments = parse_gcode(input_path.read_text())
+    first_print_segments = []
+    second_print_segments = []
+
+    for segment in segments:
         if segment.layer == "unset":
-            first_layer_segments.append(copy.deepcopy(segment))
-
-        if segment.layer == 0:
-            first_layer_segments.append(copy.deepcopy(segment))
-            if not keep_skirt or segment.type != "SKIRT":
-                print(f"removing {segment}")
-                segment.lines = [f"G92 E{segment.last_e_position}"]
+            # put start and stop sequences in both outputs
+            first_print_segments.append(replace(segment))
+            second_print_segments.append(replace(segment))
+        elif keep_skirt and segment.type == "SKIRT":
+            # put skirt in both outputs if configured
+            first_print_segments.append(replace(segment))
+            second_print_segments.append(replace(segment))
+        elif segment.layer <= split_at:
+            # put in first print, in second print set the current E position to the segments end e position so
+            # that the next move instruction won't have to over extrude the plastic to get to that position.
+            first_print_segments.append(replace(segment))
+            second_print_segments.append(
+                replace(segment, lines=[f"G92 E{segment.last_e_position}"])
+            )
         else:
-            print(f"keeping {segment}")
+            second_print_segments.append(replace(segment))
 
-    output = input_path.with_stem(f"{input_path.stem}_other_layers")
-    output.write_text("\n".join(to_gcode(main_segments)))
+    max_segment = max(
+        segment.layer for segment in second_print_segments if segment.layer != "unset"
+    )
 
-    output = input_path.with_stem(f"{input_path.stem}_first_layer")
-    output.write_text("\n".join(to_gcode(first_layer_segments)))
+    output = input_path.with_stem(
+        f"{input_path.stem}{format_attrs(layer_start=split_at + 1, layer_end=max_segment, with_skirt=keep_skirt)}"
+    )
+    output.write_text(to_gcode(first_print_segments))
+
+    output = input_path.with_stem(
+        f"{input_path.stem}{format_attrs(layer_start=0, layer_end=split_at, with_skirt=keep_skirt)}"
+    )
+    output.write_text(to_gcode(second_print_segments))
 
 
 if __name__ == "__main__":
